bde48319dfb893e85498d116d52c2c3477e38e31
SudoMakeMeASandwichDE <sudosandwichdeveloper@gmail.com>
2024-09-07 14:19:12 +0200

    1.4.0

--- ollama-cli.py
+++ ollama-cli.py
@@ -1,3 +1,20 @@
+# Made in Germany
+"""
+This file is part of ollama-cli.
+
+ollama-cli is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ollama-cli is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ollama-cli. If not, see <http://www.gnu.org/licenses/>.
+"""
 import ollama
 import os
 import ast
